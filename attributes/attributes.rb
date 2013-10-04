78bc8c9501eec2a00d8b89a9501157e4b7123c23
artdaw <artdaw@gmail.com>
2013-10-04 14:08:04 +0300

    url fixed

--- attributes/attributes.rb
+++ attributes/attributes.rb
@@ -1,10 +1,10 @@
 #
 # Cookbook Name:: vertx 
 # Attributes:: default
-#
-# Author 2012 Ryley Gahagan (rgahagan@bloomhealthco.com)
 # 
 #
 default[:vertx][:version]    = '2.1.0'
 default[:vertx][:home]       = '/usr/local/vertx'
 default[:vertx][:url]        = 'https://github.com/artdaw/packages/raw/master/vert.x/vert.x-'
+
+
